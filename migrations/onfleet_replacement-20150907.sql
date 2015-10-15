0dc8afb6f0d73f2fb3d10012319697710b3e9162
Vincent Cardillo <vcardillo@gmail.com>
2015-10-15 13:25:53 -0700

    Update onfleet_replacement-20150907.sql
    + Modifying the SQL a bit to correct some syntax errors.
    + Adding an API user table especially for Houston.
    + Adding a UNIQUE constraint on the api_token fields.

--- migrations/onfleet_replacement-20150907.sql
+++ migrations/onfleet_replacement-20150907.sql
@@ -1,16 +1,24 @@
 ALTER TABLE `Driver`
 ADD COLUMN `password` VARCHAR(255) NULL DEFAULT NULL,
 ADD COLUMN `api_token` VARCHAR(255) NULL DEFAULT NULL,
-ADD COLUMN `order_queue` VARCHAR(999) NULL DEFAULT NULL; /* 19 (digits for max long) * 50 (order IDs) + 49 (commas) = 999 */
+ADD COLUMN `order_queue` VARCHAR(999) NULL DEFAULT NULL, /* 19 (digits for max long) * 50 (order IDs) + 49 (commas) = 999 */
+ADD UNIQUE INDEX `api_token_UNIQUE` (`api_token` ASC);
 
 ALTER TABLE `admin_User`
-ADD COLUMN `api_token` VARCHAR(255) NULL DEFAULT NULL;
+ADD COLUMN `api_token` VARCHAR(255) NULL DEFAULT NULL,
+ADD UNIQUE INDEX `api_token_UNIQUE` (`api_token` ASC);
 
-ALTER TABLE `api_User`
-ADD COLUMN `api_token` VARCHAR(255) NULL DEFAULT NULL;
+CREATE TABLE `bento`.`houston_api_User` (
+  `pk_houston_api_User` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `comment` VARCHAR(255) NULL DEFAULT NULL,
+  `api_token` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`pk_houston_api_User`),
+  UNIQUE INDEX `api_token_UNIQUE` (`api_token` ASC));
+
 
 ALTER TABLE `OrderStatus`
-MODIFY COLUMN `status` ENUM('Open', 'En Route', 'Delivered', 'Cancelled', 'Rejected', 'Assigned', 'Modified') NULL DEFAULT('Open');
+CHANGE `status` `status` ENUM('Open', 'En Route', 'Delivered', 'Cancelled', 'Rejected', 'Assigned', 'Modified') NULL DEFAULT 'Open';
 
 ALTER TABLE `OrderStatusLog`
-MODIFY COLUMN `status` ENUM('Open', 'En Route', 'Delivered', 'Cancelled', 'Rejected', 'Assigned', 'Modified') NULL;
+CHANGE `status` `status` ENUM('Open', 'En Route', 'Delivered', 'Cancelled', 'Rejected', 'Assigned', 'Modified') NULL DEFAULT NULL;
