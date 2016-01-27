b045780124b92e0ad68170373ce9c67f63cf3f53
Vincent Cardillo <vcardillo@gmail.com>
2016-01-27 23:35:23 +0000

    Done on prod.

--- database/migrations/vjc-20160105_OrderAhead.sql
+++ database/migrations/vjc-20160105_OrderAhead.sql
@@ -1,5 +1,13 @@
 
 
+
+
+
+## -----------------------------------------------------------------------------------------------------------------
+# All below have been run on all! Local, dev, and prod!! 
+## -----------------------------------------------------------------------------------------------------------------
+
+
 # The following have been run on ALL
 
 # 1
@@ -67,6 +75,7 @@ CHANGE COLUMN `delivery_timezone` `scheduled_timezone` VARCHAR(45) NULL DEFAULT 
 CHANGE COLUMN `fk_Kitchen` `fk_Kitchen` INT(11) NULL DEFAULT NULL COMMENT 'Typically associated with order-ahead and/or scheduled delivery.' ,
 CHANGE COLUMN `fk_Menu` `fk_Menu` INT(11) NULL DEFAULT NULL COMMENT 'Typically associated with order-ahead and/or scheduled delivery.' ;
 
+
 # 5
 ALTER TABLE `bento`.`Order` 
 ADD COLUMN `for_date` DATE NULL DEFAULT NULL AFTER `order_type`;
@@ -75,18 +84,35 @@ ALTER TABLE `bento`.`OrderLog`
 ADD COLUMN `for_date` DATE NULL DEFAULT NULL AFTER `order_type`;
 
 
-
-# All have been run on dev only
-## -------- For dev and prod only (do after everything else) ----------------------------------------------------------
-
-INSERT INTO `bento`.`Area` (`name`, `polygon`) VALUES ('Bay Area', '-123.140258,38.104305 -121.821899,38.087013 -121.206665,37.020098 -122.332763,36.760891 -123.140258,38.104305');
-
-INSERT INTO `bento`.`OrderAheadZone` (`name`, `polygon`, `fk_Area`, `fk_Kitchen`) VALUES ('San Francisco', '-122.404003,37.813174 -122.380571,37.786453 -122.376794,37.750629 -122.395462,37.749747 -122.404346,37.74917 -122.408208,37.748322 -122.422585,37.747982 -122.438249,37.747134 -122.438303,37.748585 -122.441596,37.748593 -122.443614,37.747142 -122.444032,37.747533 -122.444161,37.748355 -122.443227,37.750247 -122.442348,37.753878 -122.44106,37.755956 -122.441639,37.756567 -122.442626,37.757093 -122.443163,37.757788 -122.443914,37.758263 -122.444815,37.758874 -122.444794,37.759502 -122.444493,37.759841 -122.443785,37.760265 -122.442648,37.761062 -122.441103,37.761079 -122.441425,37.762148 -122.446789,37.761809 -122.447068,37.762827 -122.446103,37.764302 -122.445845,37.764591 -122.446392,37.767203 -122.451247,37.766601 -122.452991,37.76638 -122.453935,37.766266 -122.453232,37.767585 -122.453479,37.768789 -122.453967,37.771198 -122.454675,37.774734 -122.455587,37.779559 -122.456027,37.780144 -122.456306,37.781289 -122.456617,37.781416 -122.456842,37.783952 -122.451816,37.784121 -122.447905,37.785041 -122.446424,37.785363 -122.447926,37.792812 -122.450952,37.80897 -122.404003,37.813174', '1', '1');
+# 6
+CREATE INDEX `idx_scheduled_window_start` ON `Order` (scheduled_window_start);
+CREATE INDEX `idx_scheduled_window_end`   ON `Order` (scheduled_window_end);
 
 
+# 7
+ALTER TABLE `bento`.`Order` 
+ADD COLUMN `utc_scheduled_window_start` DATETIME NULL DEFAULT NULL AFTER `scheduled_window_end`,
+ADD COLUMN `utc_scheduled_window_end` DATETIME NULL DEFAULT NULL AFTER `utc_scheduled_window_start`;
+
+ALTER TABLE `bento`.`OrderLog` 
+ADD COLUMN `utc_scheduled_window_start` DATETIME NULL DEFAULT NULL AFTER `scheduled_window_end`,
+ADD COLUMN `utc_scheduled_window_end` DATETIME NULL DEFAULT NULL AFTER `utc_scheduled_window_start`;
 
 
-# All have been run on local and dev only
+# 8
+ALTER TABLE `bento`.`Order` 
+CHANGE COLUMN `utc_scheduled_window_start` `utc_scheduled_window_start` TIMESTAMP NULL DEFAULT NULL ,
+CHANGE COLUMN `utc_scheduled_window_end` `utc_scheduled_window_end` TIMESTAMP NULL DEFAULT NULL ;
+
+ALTER TABLE `bento`.`OrderLog` 
+CHANGE COLUMN `utc_scheduled_window_start` `utc_scheduled_window_start` TIMESTAMP NULL DEFAULT NULL ,
+CHANGE COLUMN `utc_scheduled_window_end` `utc_scheduled_window_end` TIMESTAMP NULL DEFAULT NULL ;
+
+
+
+
+## -----------------------------------------------------------------------------------------------------------------
+# All have been run on all! Local, dev, and prod!! 
 ## -------- For All ------------------------------------------------------------------------------------------------
 
 
@@ -113,7 +139,7 @@ ALTER TABLE `bento`.`Kitchen`
 ADD COLUMN `tzName` VARCHAR(45) NULL AFTER `description`;
 
 UPDATE `bento`.`Kitchen` SET `tzName`='America/Los_Angeles' WHERE `pk_Kitchen`='1';
-
+# (DONE)
 
 
 ## Area
@@ -130,6 +156,7 @@ COMMENT = 'A conceptual service area, akin to a \"market\" or a \"region\", that
 
 ALTER TABLE `bento`.`Area` 
 CHANGE COLUMN `pk_Area` `pk_Area` INT(11) NOT NULL AUTO_INCREMENT ;
+# (DONE)
 
 
 ## OrderAheadZone
@@ -143,6 +170,7 @@ CREATE TABLE `bento`.`OrderAheadZone` (
 
 ALTER TABLE `bento`.`OrderAheadZone` 
 ADD UNIQUE INDEX `name_UNIQUE` (`name` ASC);
+# (DONE)
 
 
 ## MenuInventory
@@ -158,8 +186,8 @@ CREATE TABLE `bento`.`MenuInventory` (
   PRIMARY KEY (`pk_MenuInventory`),
   UNIQUE INDEX `uq_item` (`fk_Menu` ASC, `fk_item` ASC, `item_type` ASC),
   INDEX `index3` (`fk_Menu` ASC));
-USE `bento`;
 
+USE `bento`;
 DELIMITER $$
 DROP TRIGGER IF EXISTS bento.MenuInventoryLog$$
 USE `bento`$$
@@ -174,9 +202,8 @@ from `MenuInventory` t where pk_MenuInventory = OLD.pk_MenuInventory;
 
 END$$
 DELIMITER ;
+
 USE `bento`;
-
-
 DELIMITER $$
 DROP TRIGGER IF EXISTS bento.MenuInventoryLog_BEFORE_DELETE$$
 USE `bento`$$
@@ -191,6 +218,7 @@ from `MenuInventory` t where pk_MenuInventory = OLD.pk_MenuInventory;
 
 END$$
 DELIMITER ;
+# (DONE)
 
 
 ## MenuInventoryLog
@@ -207,6 +235,7 @@ CREATE TABLE `bento`.`MenuInventoryLog` (
   PRIMARY KEY (`pk_MenuInventoryLog`),
   INDEX `index3` (`fk_Menu` ASC));
 USE `bento`;
+# (DONE)
 
 
 ## -------- Everything else -----------------
@@ -228,6 +257,7 @@ ADD COLUMN `oa_times` TEXT NULL AFTER `oa_avail`;
 ALTER TABLE `bento`.`Menu` 
 ADD COLUMN `fk_Kitchen` INT NULL DEFAULT 1 AFTER `fk_MealType`;
 # /Menu
+# (DONE)
 
 
 # MealType
@@ -249,6 +279,8 @@ UPDATE `bento`.`MealType` SET `startTime`='10:45:00', `displayStartTime`='11:00:
 
 UPDATE `bento`.`MealType` SET `startTime`='16:45:00', `displayStartTime`='17:00:00' WHERE `pk_MealType`='3';
 # /MealType
+# (DONE)
+
 
 # Dish
 ALTER TABLE `bento`.`Dish` 
@@ -258,13 +290,13 @@ ADD COLUMN `oa_avail` TINYINT NULL DEFAULT 1 AFTER `od_avail`;
 
 
 
+## --------------------------------------------------------------------------------------------------------------------
+# All have been run on all!!
+## -------- For dev and prod only (do after everything else) ----------------------------------------------------------
 
+INSERT INTO `bento`.`Area` (`name`, `polygon`) VALUES ('Bay Area', '-123.140258,38.104305 -121.821899,38.087013 -121.206665,37.020098 -122.332763,36.760891 -123.140258,38.104305');
 
-
-
-
-
-
+INSERT INTO `bento`.`OrderAheadZone` (`name`, `polygon`, `fk_Area`, `fk_Kitchen`) VALUES ('San Francisco', '-122.404003,37.813174 -122.380571,37.786453 -122.376794,37.750629 -122.395462,37.749747 -122.404346,37.74917 -122.408208,37.748322 -122.422585,37.747982 -122.438249,37.747134 -122.438303,37.748585 -122.441596,37.748593 -122.443614,37.747142 -122.444032,37.747533 -122.444161,37.748355 -122.443227,37.750247 -122.442348,37.753878 -122.44106,37.755956 -122.441639,37.756567 -122.442626,37.757093 -122.443163,37.757788 -122.443914,37.758263 -122.444815,37.758874 -122.444794,37.759502 -122.444493,37.759841 -122.443785,37.760265 -122.442648,37.761062 -122.441103,37.761079 -122.441425,37.762148 -122.446789,37.761809 -122.447068,37.762827 -122.446103,37.764302 -122.445845,37.764591 -122.446392,37.767203 -122.451247,37.766601 -122.452991,37.76638 -122.453935,37.766266 -122.453232,37.767585 -122.453479,37.768789 -122.453967,37.771198 -122.454675,37.774734 -122.455587,37.779559 -122.456027,37.780144 -122.456306,37.781289 -122.456617,37.781416 -122.456842,37.783952 -122.451816,37.784121 -122.447905,37.785041 -122.446424,37.785363 -122.447926,37.792812 -122.450952,37.80897 -122.404003,37.813174', '1', '1');
 
 
 
